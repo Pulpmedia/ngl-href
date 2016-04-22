6fb5a2265c19b33185b1d98d259050e43ca148b9
Alexandre Kilian <alexandre@pulpmedia.at>
2016-04-22 10:15:24 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+nglHref is a directive to use $location inside a template. this way, it doesn't matter, if you're using the html5Mode or not.
+
+### Using nglHref
+
+```
+<a ngl-href="/home"></a>
+```
+results in
+
+```
+<a href="/home"></a>
+```
+
+klicking the link will trigger ```$location.path()``` instead, wich makes it independent from the environment.
---
+++ dist/ngl-href.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name nglHref.directive:nglHref
+ * @description This directive provides a  method to navigate in an application, htmlmode or not
+ * # nglHref
+ */
+ /*commonjs support*/
+ if (typeof module !== "undefined" && typeof exports !== "undefined" && module.exports === exports){
+      module.exports = 'nglHref';
+}
+
+angular.module('nglHref',[])
+  .directive('nglHref', ['$location',function (location) {
+    return {
+      restrict: 'A',
+      scope: {
+        path: '@nglHref'
+      },
+      link: function postLink(scope, element, attrs) {
+        attrs.$set('href',scope.path);
+        element[0].onclick = function(evt){
+          evt.preventDefault();
+          location.path(scope.path);
+          scope.$apply();
+        }
+      }
+    };
+  }]);
